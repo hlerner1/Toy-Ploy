78e9a4a65fb0cd4983dd4fbe13706208e5aa5429
Cody Richter <crichter@umass.edu>
2021-12-01 13:39:59 -0500

    Additional Scene Work + Lore Page

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -8,16 +8,22 @@ public class Title : MonoBehaviour
 {
 
     public Button startGameButton;
+    public Button showGameStoryButton;
     public Button showInstructionsButton;
 
     // Start is called before the first frame update
     void Start() {
         startGameButton.onClick.AddListener(() => { StartGame(); });
+        showGameStoryButton.onClick.AddListener(() => { ShowStory(); });
         showInstructionsButton.onClick.AddListener(() => { LoadInstructions(); });
     }
 
     void StartGame() {
         SceneManager.LoadScene("Game");
+    }
+
+    void ShowStory() {
+        SceneManager.LoadScene("Storyline");
     }
 
     void LoadInstructions() {
