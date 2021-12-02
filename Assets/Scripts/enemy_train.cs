6069226283a85ad3359fd9b37995911eb8eb364a
Hannah Lerner <hannahlerner@Hannahs-MacBook-Pro.local>
2021-12-01 23:40:03 -0500

    added toy prefabs and worked on tutorial level

---
+++ Assets/Scripts/enemy_train.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_train : MonoBehaviour
+{
+    private float train_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
